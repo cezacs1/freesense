4662bf653075d512cbdde926c179b37aa2010153
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-12-11 04:53:31 +0300

    Create RunVM.cs

---
+++ RunVM.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        new 612AAAAF().5138C210(new object[]
+        {
+                this
+        }, 51585);
+    }
+}
